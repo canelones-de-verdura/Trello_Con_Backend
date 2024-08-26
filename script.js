7a9aaf6cd3d1fb012dfbcc202ba0152cfa3fe753
maruuroman <maria.roman@correo.ucu.edu.uy>
2024-08-26 08:55:14 -0300

    Agregar tareas

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ theme_switch.addEventListener("click", () => {
 });
 
 // Modal
+/*
 const add_task = document.getElementById("add-task");
 add_task.addEventListener("click", () => {
     document.getElementsByClassName("modal")[0].classList.add("is-active");
@@ -24,4 +25,48 @@ add_task.addEventListener("click", () => {
 const exit_modal = document.getElementsByClassName("modal-background")[0];
 exit_modal.addEventListener("click", () => {
     document.getElementsByClassName("modal")[0].classList.remove("is-active");
+});
+*/
+
+// Obtener referencias a los elementos del DOM
+const addTaskButton = document.getElementById("add-task");
+const modal = document.getElementsByClassName("modal")[0];
+const cancelModalButton = modal.querySelector(".delete");
+const acceptModalButton = modal.querySelector(".is-success");
+const backlogColumn = document.getElementById("backlog");
+
+// Mostrar el modal cuando se haga clic en "agregar tarea"
+addTaskButton.addEventListener("click", () => {
+    modal.classList.add("is-active");
+});
+
+// Ocultar el modal al hacer clic en cancelar o fuera del modal
+cancelModalButton.addEventListener("click", () => {
+    modal.classList.remove("is-active");
+});
+
+modal.querySelector(".modal-background").addEventListener("click", () => {
+    modal.classList.remove("is-active");
+});
+
+// Crear y agregar la nueva tarea cuando se haga clic en "Aceptar"
+acceptModalButton.addEventListener("click", () => {
+    const title = modal.querySelector("input[type='text']").value;
+    const description = modal.querySelectorAll("input[type='text']")[1].value;
+    
+    // Crear un nuevo div para la tarea
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("tarea", "box", "has-background-info");
+    
+    // Añadir el título y la descripción a la tarea
+    taskDiv.innerHTML = `<strong>${title}</strong><p>${description}</p>`;
+    
+    // Añadir la tarea a la columna de Backlog
+    backlogColumn.appendChild(taskDiv);
+
+    /*
+    // Limpiar y cerrar el modal
+    modal.querySelectorAll("input[type='text']").forEach(input => input.value = "");
+    modal.classList.remove("is-active");
+    */
 });
