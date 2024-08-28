28f8044091eb11dff09f2b7177e096df36c4c089
Eze <eze.gonzalez@vera.com.uy>
2024-08-28 01:00:10 -0300

    we objects now

--- modal.js
+++ modal.js
@@ -1,141 +1,102 @@
+// TODO leer los inputs del usuario
 /***
  * SUJETO A CAMBIOS
- * Agrega el cuerpo a un modal, lo muestra
- * y deja al doc "escuchando" las acciones
- * del usuario.
+ * Clase que "envuelve" el modal.
+ * El único objetivo es estructurar la lógica.
  */
-//function spawn_modal(task) {
-//    const modal_template = `
-//        <div class="modal-background" id="background" ></div>
-//        <div class="modal-card">
-//            <header class="modal-card-head">
-//                <p class="modal-card-title"></p>
-//                <button class="delete" aria-label="close" id="close-modal-button"></button>
-//            </header>
-//            <section class="modal-card-body">
-//                <div class="block is-flex is-flex-direction-row is-justify-content-space-around">
-//                    <form id="task-form">
-//                <div class="field">
-//                    <label class="label">Título</label>
-//                    <div class="control">
-//                        <input class="input" type="text" id="titulo" placeholder="Título de la tarea">
-//                    </div>
-//                </div>
-//                <div class="field">
-//                    <label class="label">Descripción</label>
-//                    <div class="control">
-//                        <input class="input" type="text" id="descripcion" placeholder="Descripción de la tarea" maxlength="150">
-//                    </div>
-//                    <p class="help"><span id="char-count">150</span> caracteres máximo.</p>
-//                </div>
-//                <div class="field">
-//                    <label class="label">Asignado</label>
-//                    <div class="control">
-//                        <div class="select">
-//                        <select id="asignado">
-//                                <option value="">Asigne una persona</option>
-//                                <option value="maría">María</option>
-//                                <option value="media">Rafael</option>
-//                                <option value="ezequiel">Ezequiel</option>
-//                                <option value="bruno">Bruno</option>
-//                                <option value="Germán">Germán</option>
-//                            </select>
-//                        </div>
-//                    </div>
-//                </div>
-//                <div class="field">
-//                    <label class="label">Prioridad</label>
-//                    <div class="control">
-//                        <div class="select">
-//                            <select id="prioridad">
-//                                <option value="">Seleccione una Prioridad</option>
-//                                <option value="Baja">Baja</option>
-//                                <option value="Media">Media</option>
-//                                <option value="Alta">Alta</option>
-//                            </select>
-//                        </div>
-//                    </div>
-//                </div>
-//                <div class="field">
-//                        <label class="label">Estado</label>
-//                        <div class="control">
-//                            <div class="select">
-//                                <select name="status" required id="estado">
-//                                    <option value="">Seleccione un Estado</option>
-//                                    <option value="Backlog">Backlog</option>
-//                                    <option value="To-Do">To-Do</option>
-//                                    <option value="In-Progress">In-Progress</option>
-//                                    <option value="Blocked">Blocked</option>
-//                                    <option value="Done">Done</option>
-//                                </select>
-//                            </div>
-//                        </div>
-//                    </div>
-//                <div class="field">
-//                    <label class="label" id="fecha limite">Fecha límite</label>
-//                    <div class="control">
-//                        <input class="input" id="fecha limite" type="date">
-//                    </div>
-//                </div>
-//            </form>
-//            </section>
-//            <footer class="modal-card-foot">
-//                <div class="buttons">
-//                    <button class="button cancel">Cancelar</button>
-//                    <button class="button is-success">Aceptar</button>
-//                </div>
-//            </footer>
-//        </div>
-//    `;
-//
-//    const modal = document.getElementById("task-menu");
-//    modal.innerHTML = modal_template;
-//    return modal;
-//}
+class taskMenu {
+    /***
+     * @param div que se va a utilizar como modal
+     */
+    constructor(HTML_Element) {
+        // Atributos que vale la pena tener a mano
+        this.element = HTML_Element;
+        this.title = HTML_Element.querySelector(".modal-card-title");
+        this.btn_holder = HTML_Element.querySelector(".buttons"); // 0 = cancelar, 1 = aceptar, 2 = eliminar tarjeta
 
-/***
- * SUJETO A CAMBIOS
- * Dado un modal ya creado, recupera sus elementos,
- * configura los eventos para "escuchar" y lo muestra.
- *
- * @param div necesariamente debe ser el que devuelve spawn_modal().
- * @param task si null, crea una nueva
- */
-function setup_modal(modal, task) {
-    // Recuperamos elementos del doc
-
-    const modal_background = document.getElementById("background");
-    const acceptModalButton = modal.querySelector(".is-success");
-    const cancelModalButton = modal.querySelector(".cancel");
-
-    // Configuramos eventos
-    modal_background.addEventListener("click", () => {
-        modal.classList.remove("is-active");
-    });
-
-    cancelModalButton.addEventListener("click", () => {
-        // modal.innerHTML = "";
-        modal.classList.remove("is-active");
-    });
-
-    // agregamos una tarea nueva
-    if (task === null) {
-        // primero agregamos header
-        modal.getElementsByClassName("modal-card-title")[0].innerHTML = "Agregar Tarea"
-
-        // Crear y agregar la nueva tarea
-        acceptModalButton.addEventListener("click", () => {
-            const title = modal.querySelector("input").value;
-            const description = modal.querySelectorAll("input")[1].value;
-
-            new_task(title, description, null, null, null);
-
-            // Limpiar y cerrar el modal
-            modal.querySelectorAll("input").forEach(input => input.value = "");
-            modal.classList.remove("is-active");
+        // Configuramos eventos
+        const modal_background = document.getElementById("background");
+        modal_background.addEventListener("click", () => this.close());
+        this.btn_holder.children[0].addEventListener("click", () => this.close());
+        this.element.addEventListener("keydown", ({ key }) => {
+            if (key === "Escape")
+                this.close()
         });
     }
 
-    // Mostramos el modal
-    modal.classList.add("is-active");
+    /***
+     * Termina de configurar el modal y lo muestra.
+     * @param task Tarea a editar. Si es null, se crea una tarea nueva
+     */
+    spawn(task, dashboard) {
+        let title = "";
+        const confirm_button = this.btn_holder.children[1];
+        const delete_button = this.btn_holder.children[2];
+
+        if (task.is_empty) {
+            title = "Nueva tarea";
+            confirm_button.innerText = "Agregar tarea";
+            delete_button.classList.add("is-hidden");
+
+            confirm_button.addEventListener("click", () => {
+        // Input
+        const titulo = this.element.querySelector("#titulo").value;
+        const descripcion = this.element.querySelector("#descripcion").value;
+        const asignado = this.element.querySelector("#asignado").value;
+        const prioridad = this.element.querySelector("#prioridad").value;
+        const estado = this.element.querySelector("#estado").value;
+        //const fecha_limite = this.element.querySelectorAll("#fecha_limite").value;
+        const fecha_limite = null;
+
+                console.log(titulo);
+                console.log(descripcion);
+                console.log(asignado);
+                console.log(prioridad);
+                console.log(estado);
+                console.log(fecha_limite);
+
+                task.fill(titulo, descripcion, asignado, prioridad, estado, fecha_limite);
+                dashboard.columns[0].placeCard(task);
+                this.close();
+            });
+        } else {
+            title = "Editar tarea";
+            confirm_button.innerText = "Aceptar";
+            delete_button.classList.remove("is-hidden");
+
+            delete_button.addEventListener("click", () => {
+                // TODO
+            });
+
+            confirm_button.addEventListener("click", () => {
+                this.close();
+            });
+        }
+
+        // Título
+        this.title.innerText = title;
+
+        // Mostramos modal
+        this.element.classList.add("is-active");
+    }
+
+    /*
+    recoverInput() {
+        const titulo = modal.querySelector("#titulo").value;
+        const descripcion = modal.querySelector("#descripcion").value;
+        const asignado = modal.querySelector("#asignado").value;
+        const prioridad = modal.querySelector("#prioridad").value;
+        const estado = modal.querySelector("#estado").value;
+        const fecha_limite = modal.querySelectorAll("#fecha_limite").value;
+
+    }
+    */
+
+    close() {
+        // Malabares para deshacer las event calls
+        this.btn_holder.replaceChild(this.btn_holder.children[1].cloneNode(), this.btn_holder.children[1]);
+        this.btn_holder.replaceChild(this.btn_holder.children[2].cloneNode(), this.btn_holder.children[2]);
+        // Ocultamos
+        this.element.classList.remove("is-active");
+    }
 }
--- task.js
+++ task.js
@@ -1,51 +1,51 @@
 /***
  * SUJETO A CAMBIOS
- * Crea una tarjeta nueva y la agrega en la columna del backlog.
+ * Clase que "envuelve" una tarjeta (un div con cosas adentro).
+ * El único objetivo es estructurar la lógica.
  */
-function new_task(titulo, desc, asignado, prioridad, fecha_limite) {
-    const task_template = `
-        <div class="card" draggable="true">
+class taskCard {
+    constructor() {
+        this.is_empty = true;
+        this.state = -1;
+        this.element = document.createElement("div")
+
+        this.element.classList.add("card");
+        this.element.setAttribute("draggable", "true");
+        this.element.setAttribute("id", crypto.randomUUID());
+
+        // Añadir eventos de arrastrar y soltar a la tarea, es necesario que sea cuando son creados porque sino no lo toma
+        this.element.addEventListener('dragstart', (event) => {
+            event.dataTransfer.setData('text/plain', event.target.id);
+            this.element.classList.add('dragging');
+        });
+
+        this.element.addEventListener('dragend', () => {
+            this.element.classList.remove('dragging');
+        });
+
+        // Falta evento para clickear y abrir el modal de edición.
+        // No va a acá.
+    }
+
+    // TODO hacer bien las cosas
+    /***
+     * Método para rellenar la tarjeta. Se usa el mismo al crearla por primera vez o editarla
+     */
+    fill(titulo, desc, asignado, prioridad, estado, fecha_limite) {
+        const card_template = `
             <header class="card-header">
-                <p class="card-header-title"></p>
+                <p class="card-header-title">${titulo}</p>
                 <div class="card-header-icon">
                     <span class="material-symbols-outlined">task_alt</span>
                 </div>
             </header>
             <div class="card-content">
-                <div class="content">
-                </div>
+                <div class="content">${desc}</div>
             </div>
-        </div>
-    `;
-    
-    // insertamos tarjeta vacía
-    const backlogColumn = document.getElementById("backlog");
-    backlogColumn.innerHTML += task_template;
+        `;
 
-    // recuperamos la tarjeta recién creada
-    const task_list = backlogColumn.getElementsByClassName("card"); 
-    const task = task_list[task_list.length-1];
-
-    const title_field = task.querySelector(".card-header-title");
-    const text_field = task.querySelector(".content");
-
-    // agregamos texto
-    title_field.innerHTML = `<strong>${titulo}</strong>`
-    text_field.innerHTML = `<p>${desc}</p>`;
-
-    // Asignar un ID único a la tarea
-    const uniqueId = 'task-' + new Date().getTime();
-    task.setAttribute("id", uniqueId);
-
-    // Añadir eventos de arrastrar y soltar a la tarea, es necesario que sea cuando son creados porque sino no lo toma
-    task.addEventListener('dragstart', (event) => {
-        event.dataTransfer.setData('text/plain', event.target.id);
-        task.classList.add('dragging');
-    });
-
-    task.addEventListener('dragend', () => {
-        task.classList.remove('dragging');
-    });
-
-    backlogColumn.appendChild(task);
+        this.state = estado;
+        this.element.innerHTML = card_template;
+        this.is_empty = false;
+    }
 }
