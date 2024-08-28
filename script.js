6ad47b79b1d1b2d0fb1a7baf61206924aab92d16
Bruno <132522774+TDxBruuno@users.noreply.github.com>
2024-08-28 09:11:14 -0300

    asd

--- script.js
+++ script.js
@@ -21,18 +21,11 @@ add_task_btn.addEventListener("click", () => {
     const new_task = new taskCard();
     menu.spawn(new_task, dashboard); // Rellenamos la tarea
 
-<<<<<<< HEAD
-// Modal
-const add_task = document.getElementById("add-task");
-add_task.addEventListener("click", () => {
-    document.getElementsByClassName("modal")[0].classList.add("is-active");
-=======
     // AGREGAR TARJETA A COLUMNA
     //dashboard.columns[new_task.priority].placeCard(new_task);
 
     // Agregamos el listener en la tarjeta nueva
     new_task.element.addEventListener("click", () => menu.spawn(new_task));
->>>>>>> b4a64723aaf1ed58a6571c5768cc44e93b8eed1f
 });
 
 // Cerrar el modal al hacer clic en el fondo
@@ -43,16 +36,8 @@ exit_modal.addEventListener("click", closeModal);
 const close_button = document.getElementsByClassName("delete")[0];
 close_button.addEventListener("click", closeModal)
 
-<<<<<<< HEAD
-
-
-function closeModal() {
-    document.getElementsByClassName("modal")[0].classList.remove("is-active");
-    clearform();
-=======
 function closeModal() {
   document.getElementsByClassName("modal")[0].classList.remove("is-active");
->>>>>>> b4a64723aaf1ed58a6571c5768cc44e93b8eed1f
 }
 
 // Cerrar el modal al hacer clic en el botón cancelar
@@ -60,19 +45,10 @@ const cancel_button = document.getElementById("cancel-button");
 cancel_button.addEventListener("click", closeModal);
 
 function clearform() {
-<<<<<<< HEAD
-    document.getElementById("titulo").value = "";
-    document.getElementById("descripcion").value = "";
-    document.getElementById("asignado").value = "";
-    document.getElementById("prioridad").value = "";
-    document.getElementById("estado").value = "";
-    document.getElementById("fecha limite").value = "";
-=======
   document.getElementById("titulo").value = "";
   document.getElementById("descripcion").value = "";
   document.getElementById("asignado").value = "";
   document.getElementById("prioridad").value = "";
   document.getElementById("estado").value = "";
   document.getElementById("fecha limite").value = "";
->>>>>>> b4a64723aaf1ed58a6571c5768cc44e93b8eed1f
 }
