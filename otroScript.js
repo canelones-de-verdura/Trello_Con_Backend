602a0cf5962a5235600dd5c9265e1bb4f1de215d
adjem <german.adjemian@correo.ucu.edu.uy>
2024-08-27 22:15:12 -0300

    MUCHOS CAMBIOS, de estilo, en el drag and drop y en la funcion de creacion de cartas, luego habría que ver un poco las clases de las columnas que creo que hay demas

--- otroScript.js
+++ otroScript.js
@@ -1,6 +1,6 @@
 // Obtener todas las tareas y columnas
-const cells = document.querySelectorAll('.tarea');
-const tables = document.querySelectorAll('.column');
+const cells = document.querySelectorAll('.card');
+const tables = document.querySelectorAll('.columna');
 
 // Comentado porque las tareas se crean dinámicamente
 /*cells.forEach(cell => {
@@ -24,7 +24,14 @@ tables.forEach(table => {
     table.addEventListener('drop', (event) => {
         event.preventDefault();
         const draggedElementId = event.dataTransfer.getData('text/plain'); // Obtener el ID del elemento arrastrado
+        console.log("El id es: "+draggedElementId);
+        if(draggedElementId){
+            console.log(typeof(draggedElementId));
+        }
+        //console.log(typeof(draggedElementId));
         const draggedElement = document.getElementById(draggedElementId); // Buscar el elemento arrastrado por su ID
+        console.log("puede ser que en el id de arriba no imprime nada pero aun asi no se corta la ejecucion");
+        console.log(draggedElement);
 
         if (draggedElement) {
             table.appendChild(draggedElement); // Mover el elemento arrastrado a la nueva columna
