68a088439f2c737f6072edc4e24ac11ac7518508
Bruno <132522774+TDxBruuno@users.noreply.github.com>
2024-09-02 01:03:39 -0300

    ultimos cambios

--- classes/task.js
+++ classes/task.js
@@ -4,54 +4,53 @@
  * El único objetivo es estructurar la lógica.
  */
 class taskCard {
-    constructor() {
-        // Indica si la tarjeta está recién creada
-        this.is_empty = true;
+  constructor() {
+    // Indica si la tarjeta está recién creada
+    this.is_empty = true;
 
-        // Info de la tarea
-        this.titulo = null;
-        this.desc = null;
-        this.asignado = null;
-        this.prioridad = null;
-        this.estado = null;
-        this.fecha_limite = null;
+    // Info de la tarea
+    this.titulo = null;
+    this.desc = null;
+    this.asignado = null;
+    this.prioridad = null;
+    this.estado = null;
+    this.fecha_limite = null;
 
+    // Elemento HTML, con sus atributos correspondientes
+    this.element = document.createElement("div");
+    this.element.classList.add("card");
+    this.element.setAttribute("draggable", "true");
+    this.element.setAttribute("id", crypto.randomUUID());
 
-        // Elemento HTML, con sus atributos correspondientes
-        this.element = document.createElement("div")
-        this.element.classList.add("card");
-        this.element.setAttribute("draggable", "true");
-        this.element.setAttribute("id", crypto.randomUUID());
+    // Añadir eventos de arrastrar y soltar a la tarea, es necesario que sea cuando son creados porque sino no lo toma
+    this.element.addEventListener("dragstart", (event) => {
+      event.dataTransfer.setData("text/plain", event.target.id);
+      this.element.classList.add("dragging");
+    });
 
-        // Añadir eventos de arrastrar y soltar a la tarea, es necesario que sea cuando son creados porque sino no lo toma
-        this.element.addEventListener('dragstart', (event) => {
-            event.dataTransfer.setData('text/plain', event.target.id);
-            this.element.classList.add('dragging');
-        });
+    this.element.addEventListener("dragend", () => {
+      this.element.classList.remove("dragging");
 
-        this.element.addEventListener('dragend', () => {
-            this.element.classList.remove('dragging');
+      // actualizamos estado
+      this.estado = this.element.parentNode.getAttribute("id");
+    });
+  }
 
-            // actualizamos estado
-            this.estado = this.element.parentNode.getAttribute("id");
-        });
-    }
+  // TODO hacer bien la template, que se ve como el tuje
+  /***
+   * Método para rellenar la tarjeta. Se usa el mismo al crearla por primera vez o editarla
+   */
+  fill(titulo, desc, asignado, prioridad, estado, fecha_limite) {
+    // Llenamos los atributos
+    this.titulo = titulo;
+    this.desc = desc;
+    this.asignado = asignado;
+    this.prioridad = prioridad;
+    this.estado = estado;
+    this.fecha_limite = fecha_limite;
 
-    // TODO hacer bien la template, que se ve como el tuje
-    /***
-     * Método para rellenar la tarjeta. Se usa el mismo al crearla por primera vez o editarla
-     */
-    fill(titulo, desc, asignado, prioridad, estado, fecha_limite) {
-        // Llenamos los atributos
-        this.titulo = titulo;
-        this.desc = desc;
-        this.asignado = asignado;
-        this.prioridad = prioridad;
-        this.estado = estado;
-        this.fecha_limite = fecha_limite;
-
-        // Llenamos el html
-        this.element.innerHTML = `
+    // Llenamos el html
+    this.element.innerHTML = `
             <header class="card-header">
                 <p class="card-header-title">${titulo}</p>
                 <div class="card-header-icon">
@@ -75,20 +74,41 @@ class taskCard {
             </div>
         `;
 
-        // Coloreamos el tag con la prioridad
-        switch (prioridad) {
-            case "Alta":
-                this.element.querySelector("#prioridad").classList.add("is-danger");
-            case "Media":
-                this.element.querySelector("#prioridad").classList.add("is-warning");
-            case "Baja":
-                this.element.querySelector("#prioridad").classList.add("is-success");
-        }
-        // Marcamos la tarjeta como rellena
-        this.is_empty = false;
+    // Coloreamos el tag con la prioridad
+    switch (prioridad) {
+      case "Alta":
+        this.element.querySelector("#prioridad").classList.add("is-danger");
+      case "Media":
+        this.element.querySelector("#prioridad").classList.add("is-warning");
+      case "Baja":
+        this.element.querySelector("#prioridad").classList.add("is-success");
     }
+    // Marcamos la tarjeta como rellena
+    this.is_empty = false;
+  }
 
-    delete() {
-        this.element.parentNode.removeChild(this.element);
-    }
+  delete() {
+    this.element.parentNode.removeChild(this.element);
+  }
 }
+
+const columns = document.querySelectorAll(".custom-column");
+
+columns.forEach((column) => {
+  column.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    column.classList.add("drag-over"); // Añade la clase 'drag-over' al arrastrar
+  });
+
+  column.addEventListener("dragleave", () => {
+    column.classList.remove("drag-over"); // Elimina la clase 'drag-over' al salir
+  });
+
+  column.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData("text/plain");
+    const draggedElement = document.getElementById(id);
+    column.classList.remove("drag-over"); // Elimina la clase 'drag-over' al soltar
+    column.appendChild(draggedElement);
+  });
+});
