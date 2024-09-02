7945067f4f5fc74f43d2a9425fa673cfef084fd3
Eze <eze.gonzalez@vera.com.uy>
2024-09-02 10:18:10 -0300

    ?

--- classes/dashboard.js
+++ classes/dashboard.js
@@ -4,11 +4,6 @@ class dashboardColumn {
         this.element = document.createElement("div");
         this.element.classList.add(
             "custom-column"
-            //"column",
-            //"has-background-light",
-            //"is-flex",
-            //"is-flex-direction-column",
-            //"is-align-items-center"
         );
         this.element.setAttribute("id", id);
         this.element.innerHTML = `<h2 class="subtitle is-3">${name}</h2>`;
@@ -34,22 +29,6 @@ class dashboardColumn {
                 console.error('El elemento arrastrado no existe en el DOM:', dragged_element_id);
             }
         });
-        //// Configuramos eventos
-        //this.element.addEventListener('dragover', (event) => {
-        //    event.preventDefault(); // Necesario para permitir el drop
-        //});
-        //
-        //this.element.addEventListener('drop', (event) => {
-        //    event.preventDefault();
-        //    const dragged_element_id = event.dataTransfer.getData('text/plain'); // Obtener el ID del elemento arrastrado
-        //    const dragged_element = document.getElementById(dragged_element_id); // Buscar el elemento arrastrado por su ID
-        //
-        //    if (dragged_element) {
-        //        this.element.appendChild(dragged_element); // Mover el elemento arrastrado a la nueva columna
-        //    } else {
-        //        console.error('El elemento arrastrado no existe en el DOM:', dragged_element_id);
-        //    }
-        //});
     }
 }
 
@@ -79,29 +58,14 @@ class taskDashboard {
     }
 
     addTask(task, state) {
-        
         this.contents.push(task)
         this[state].element.querySelector(".inner-column").appendChild(task.element);
-        
-        //// Evento para abrir el modal de edición.
-        //this.element.querySelector(".card-content").addEventListener("click", () => modal.spawn(task));
-
-        // Evento para eliminar la tarea.
-        //task.element.querySelector(".card-header-icon").addEventListener("click", () => this.deleteTask(task));
-    
     }
 
     deleteTask(task) {
         if (this.contents.includes(task)) {
             task.delete();
             this.contents = this.contents.filter(keep => keep !== task);
-            /*
-            // debug
-            if (this.contents.includes(task))
-                console.log("No se borró!");
-            */
-            // Guarda las tareas cada vez que se elimina una
-            this.saveTasks();
         }
     }
 
